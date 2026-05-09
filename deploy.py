34d7ac61318fcfa511bc96ae40b8435592fb26cc
Byazrov_Soslan <soslan-bv@mail.ru>
2026-05-09 20:05:15 +0300

    auto: update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,6 +1,9 @@
 import paramiko
 import time
 import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', errors='replace')
 
 HOST = '85.198.87.155'
 USER = 'root'
